3e8d71b490eaeefc368a4c390ec9b2b77181cd9e
Jani Turunen <janisku7@gmail.com>
2019-03-18 16:36:33 +0200

    lots of changes

---
+++ StreamKontrolUnit.BackEnd/Hubs/StreamKontrolUnitHub.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using SKU.Stream.Services;
+using StreamKernelUnit;
+using StreamKontrolUnit.BackEnd.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StreamKontrolUnit.BackEnd.Hubs
+{
+    [Authorize(JwtBearerDefaults.AuthenticationScheme)]
+    public class StreamKontrolUnitHub : BaseHub
+    {
+        private StreamService StreamService { get; }
+        private FollowerClient FollowerClient { get; }
+        private StreamKontrolUnitClient StreamKontrolUnitClient { get; }
+        private ViewerClient ViewerClient { get; }
+
+        public StreamKontrolUnitHub(StreamService streamService, FollowerClient followerClient, StreamKontrolUnitClient streamKontrolUnitClient, ViewerClient viewerClient)
+        {
+            this.StreamService = streamService;
+            this.StreamKontrolUnitClient = streamKontrolUnitClient;
+            this.FollowerClient = followerClient;
+            this.ViewerClient = viewerClient;
+            StreamService.Updated += StreamService_Updated;
+        }
+
+        private void StreamService_Updated(object sender, ServiceUpdatedEventArgs e)
+        {
+            if (e.NewFollowers.HasValue)
+            {
+                this.FollowerClient.UpdateFollowers(StreamService.CurrentFollowerCount + e.NewFollowers.Value);
+            }
+            if (e.NewViewers.HasValue)
+            {
+                this.ViewerClient.UpdateViewers(e.ServiceName,e.NewViewers.Value);
+            }
+        }
+    }
+}
---
+++ StreamKontrolUnit.BackEnd/Startup.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Blazor.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Serialization;
+using StreamKontrolUnit.BackEnd.Data;
+using StreamKontrolUnit.BackEnd.Hubs;
+using StreamKontrolUnit.BackEnd.Interface;
+using StreamKontrolUnit.BackEnd.Models;
+using StreamKontrolUnit.BackEnd.Services;
+
+namespace StreamKontrolUnit.BackEnd
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        private readonly SymmetricSecurityKey SecurityKey = new SymmetricSecurityKey(Guid.NewGuid().ToByteArray());
+        private readonly JwtSecurityTokenHandler JwtTokenHandler = new JwtSecurityTokenHandler();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR().AddJsonProtocol(options => 
+            {
+                options.PayloadSerializerSettings.ContractResolver = new DefaultContractResolver();
+                });
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+           
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+                {
+                    policy.AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme);
+                    policy.RequireClaim(ClaimTypes.NameIdentifier);
+                });
+            });
+
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            LifetimeValidator = (before, expires, token, param) =>
+            {
+
+                return expires > DateTime.UtcNow;
+            },
+            ValidateActor = false,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            IssuerSigningKey = SecurityKey
+         };
+        options.Events = new JwtBearerEvents{
+            OnMessageReceived = context =>{
+            var accessToken = context.Request.Query["access_token"];
+        var path = context.HttpContext.Request.Path;
+        if (!string.IsNullOrEmpty(accessToken)&& (path.StartsWithSegments("/hubs/chat")))
+	{
+            context.Token = accessToken;
+	}
+    return Task.CompletedTask;
+        }};
+            });
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+                {
+                    MediaTypeNames.Application.Octet,
+                    WasmMediaTypeNames.Application.Wasm,
+                });
+            });
+            
+            services.AddSingleton<IUserIdProvider, NameUserIdProvider>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseServerSideBlazor<StreamControlUnit.WebDash.Startup>();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<StreamKontrolUnitHub>("/streamkontrolunithub");
+                routes.MapHub<JT7SKUOctoHub>("/jt7skuoctohub");
+            });
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("generatetoken", c => c.Response.WriteAsync(GenerateToken(c)));
+            app.UseRouter(routeBuilder.Build());
+        }
+        private string GenerateToken(HttpContext httpContext)
+        {
+            var claims = new[] { new Claim(ClaimTypes.NameIdentifier, httpContext.Request.Query["user"]) };
+            var credentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken("StreamKontrolUnitBackend", "StreamKontrolUnitOBS", claims, expires: DateTime.UtcNow.AddSeconds(30), signingCredentials: credentials);
+            return JwtTokenHandler.WriteToken(token);
+        }
+    }
+}
